38cb0fb24b1c26a1291472f6fc7ae7740da0944e
Ssnnee <nandisamuelsne@gmail.com>
2023-12-07 17:57:44 +0100

    Cleaner code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,15 +2,15 @@
 # Script that sets up a web servers for the deployment of web_static.
 
 if ! [ -x "$(command -v nginx)" ]; then
- apt update
- apt-get -y install nginx
+apt update
+apt-get - y install nginx
 fi
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 
 echo "<h1>
-    Some random content for testing
+Some random content for testing
 </h1>" | tee /data/web_static/releases/test/index.html
 
 ln -sf /data/web_static/releases/test/ /data/web_static/current
@@ -19,12 +19,12 @@ chown -R ubuntu:ubuntu /data/
 
 bash -c 'cat << EOF > /etc/nginx/sites-available/web_static
 server {
-   listen 80;
-   server_name samuelnandi.tech;
+	listen 80;
+	server_name samuelnandi.tech;
 
-   location /hbnb_static/ {
-       alias /data/web_static/current/;
-   }
+	location /hbnb_static/ {
+		alias /data/web_static/current/;
+	}
 }
 EOF'
 
